ee306e732c93ebea2382d9f01e2416b9bb7e6c95
Anju Priya V <anjupriyachennai@gmail.com>
2023-02-05 09:17:22 +0530

    Update numeric-to-roman.java

--- numeric-to-roman.java
+++ numeric-to-roman.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class NumericToRoman {
+class NumericToRoman {
  public static void main(String[] args) {
      String[] romanValues={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
      int[] values={1000,900,500,400,100,90,50,40,10,9,5,4,1};
