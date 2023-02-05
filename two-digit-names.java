606da805ded5e730f77fe1ce817ce7e09776dcf4
Anju Priya V <anjupriyachennai@gmail.com>
2023-02-05 09:14:04 +0530

    Update and rename TwoDigitNames.java to two-digit-names.java

--- two-digit-names.java
+++ two-digit-names.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class TwoDigitNames {
+class TwoDigitNames {
     public static void main(String[] args) {
         int n,rem=0;
         Scanner sc=new Scanner(System.in);
